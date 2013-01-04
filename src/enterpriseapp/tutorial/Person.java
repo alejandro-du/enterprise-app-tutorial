6e071e18dc442a2461cdbb7f66599a1de37cbce1
jaduarteav <alejandro.d.a@gmail.com>
2013-01-04 14:38:20 -0500

    Comments added.

--- src/enterpriseapp/tutorial/Person.java
+++ src/enterpriseapp/tutorial/Person.java
@@ -8,6 +8,18 @@ import javax.persistence.Id;
 
 import enterpriseapp.hibernate.dto.Dto;
 
+/**
+ * This is our Hibernate entity class mapped just as usual. No magic here ;)
+ * ... well, maybe ther's a little. We need to extend Dto (yeah right that's
+ * not too cool, but you'll see some magic because of this) and override
+ * some getter and setter.
+ * 
+ * For the sake of completeness, check META-INF/persistence.xml and
+ * configuration.properties.
+ * 
+ * @author Alejandro Duarte
+ *
+ */
 @Entity
 public class Person extends Dto {
 	
@@ -23,20 +35,22 @@ public class Person extends Dto {
 	private String phoneNumber;
 
 	@Override
-	public Long getId() {
+	public Long getId() { // we've got to override this
 		return id;
 	}
 
 	@Override
-	public void setId(Object id) {
+	public void setId(Object id) { // and this
 		this.id = (Long) id;
 	}
-   
-	public void setId(Long id) {
+	
+	/* also... providing this setter (for Long) is good (you can tell! I've spent like a month
+	   debugging an app and solved with this little setter) */
+	public void setId(Long id) { 
 		this.id = id;
 	}
 
-	public String getName() {
+	public String getName() { // also this... I'm jus kidding XD
 		return name;
 	}
 
--- src/enterpriseapp/tutorial/TutorialEnterpriseApplication.java
+++ src/enterpriseapp/tutorial/TutorialEnterpriseApplication.java
@@ -6,18 +6,33 @@ import enterpriseapp.EnterpriseApplication;
 import enterpriseapp.ui.crud.CrudBuilder;
 import enterpriseapp.ui.crud.CrudComponent;
 
+/**
+ * This is our main application class. Here we'll need to extend
+ * EnterpriseApplication to gain all Enterprise App power and mightiness.
+ * 
+ * @author Alejandro Duarte
+ *
+ */
 public class TutorialEnterpriseApplication extends EnterpriseApplication {
+	
 	private static final long serialVersionUID = 1L;
 
 	@Override
 	public void init() {
+		// don't ever, ever, ever, EVER forget the following line. This will bring Hibernate's Goods powers
 		super.init();
+		
+		// create a full operating CRUD interface component right away!
 		CrudComponent<Person> crud = new CrudBuilder<Person>(Person.class).build();
 		crud.setSizeFull();
+		
+		// usual Vaadin stuff
 		Window mainWindow = new Window("Address Book");
 		mainWindow.setSizeFull();
 		mainWindow.setContent(crud);
 		setMainWindow(mainWindow);
+		
+		// that's it... what are you waing for? run it!
 	}
 
 }
